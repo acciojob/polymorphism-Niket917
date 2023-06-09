6d7dcb027a3f5356939516b4cf03890913d5bc53
Niket Sahu <nikkysahu8871@gmail.com>
2023-06-09 12:55:31 +0530

    This is a Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,26 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println( p.Product(3,7));
+
+        System.out.println( p.Product(3,7,23));
+
+        System.out.println( p.Product(30.0,74.5));
+    }
+
+  static  class Product{
+
+        public int Product(int x, int y){
+            return x + y;
+        }
+        public int Product(int x, int y, int z){
+            return x + y + z;
+        }
+        public double Product(double x, double y){
+            return x + y;
+        }
+
+    }
 }
