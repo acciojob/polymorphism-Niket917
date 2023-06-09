161e4c4708cad585e809646fc05fbcc3e0ff0ccc
Niket Sahu <nikkysahu8871@gmail.com>
2023-06-09 13:16:24 +0530

    Second time Change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,23 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         Product p = new Product();
-        System.out.println(p.Product(3, 7));
+        System.out.println(p.product(3, 7));
 
-        System.out.println(p.Product(3, 7, 23));
+        System.out.println(p.product(3, 7, 23));
 
-        System.out.println(p.Product(30.0, 74.5));
+        System.out.println(p.product(30.0, 74.5));
     }
 }
 
     class Product{
-
-        public int Product(int x, int y){
+        public int product(int x, int y){
             return x + y;
         }
-        public int Product(int x, int y, int z){
+        public int product(int x, int y, int z){
             return x + y + z;
         }
-        public double Product(double x, double y){
+        public double product(double x, double y){
             return x + y;
         }
 
