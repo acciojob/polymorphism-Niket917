7a9a4e4ef99c3f6a6f3c3af165d7b7f50ec80e40
Niket Sahu <nikkysahu8871@gmail.com>
2023-06-09 13:23:39 +0530

    Change another time

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,26 +1,22 @@
 package com.driver;
 
 public class Main {
-
+    public static class Product{
+        public int product(int x, int y){
+            return x+y;
+        }
+        public int product(int x, int y, int z){
+            return x+y+z;
+        }
+        public double product(double x, double y){
+            return x+y;
+        }
+    }
     public static void main(String[] args) {
         Product p = new Product();
-        System.out.println(p.product(3, 7));
-
-        System.out.println(p.product(3, 7, 23));
-
-        System.out.println(p.product(30.0, 74.5));
+        p.product(1, 2);
+        p.product(1, 2, 3);
+        p.product(2.5, 3.5);
     }
 }
 
-    class Product{
-        public int product(int x, int y){
-            return x + y;
-        }
-        public int product(int x, int y, int z){
-            return x + y + z;
-        }
-        public double product(double x, double y){
-            return x + y;
-        }
-
-    }
